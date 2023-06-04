9308bcb2e43881176ed214e72a6c5e15250cda2c
Mikee100 <99830445+Mikee100@users.noreply.github.com>
2023-06-04 22:10:57 +0300

    Add files via upload

---
+++ bank/app.js
@@ -0,0 +1,11 @@
+
+let nav = document.getElementById("nav");
+
+function openMenu(){
+    nav.classList.add("open-nav");
+
+}
+function removeCart() {
+    nav.classList.remove("open-nav");
+    
+}
